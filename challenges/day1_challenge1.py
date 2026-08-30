13038580594f8e87f7b7162830679ec934eeed75
Reva-ahmet <rewaaa20@gmail.com>
2026-08-30 15:32:29 +0300

    Add files via upload

---
+++ challenges/day1_challenge1.py
@@ -0,0 +1,7 @@
+print("Hello welcome")
+
+name = input("Enter your name: ")
+uni_name = input("Enter your UNI name: ")
+major = input("Enter your major: ")
+
+print(f"Welcome {name} your are  {major} student in  {uni_name} university.")
