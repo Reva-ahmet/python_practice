581c10b78af2f41b4c6186f8cc914007cd1a9bf0
Reva-ahmet <rewaaa20@gmail.com>
2026-08-31 16:47:37 +0300

    Add files via upload

---
+++ challenges/day1_challenge3.py
@@ -0,0 +1,9 @@
+birth_year = int(input("Enter your birth year: "))
+your_age = 2026 - birth_year
+age_in_2030 = 2030 - birth_year
+age_in_2040 = 2040 - birth_year
+
+print(f"Your birth year is {birth_year}.")
+print(f"Your current age is {your_age}.")
+print(f"Your age in 2030 will be {age_in_2030}.")
+print(f"Your age in 2040 will be {age_in_2040}.")
