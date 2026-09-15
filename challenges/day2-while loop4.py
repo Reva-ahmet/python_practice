f331ea80575deb7409882e9778eeff60278ec4ba
Reva-ahmet <rewaaa20@gmail.com>
2026-09-15 13:50:12 +0300

    Add files via upload

---
+++ challenges/day2-while loop4.py
@@ -0,0 +1,10 @@
+# while loop 4.
+
+num = int(input("Enter a number between 1 and 10: "))
+
+while num < 1 or num > 10:
+    print(f"{num} is not valid.")
+    print("Please try again!")
+    num = int(input("Enter a number between 1 and 10: "))
+
+print(f"You choose number: {num}")
